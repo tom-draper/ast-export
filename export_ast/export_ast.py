e9dd4bb659708304bec2014205ca18d144394fa0
Tom Draper <tomjdraper1@gmail.com>
2022-12-24 00:41:13 +0000

    Fix floats

--- export_ast/export_ast.py
+++ export_ast/export_ast.py
@@ -55,21 +55,25 @@ def _enclosed_calls(parsed_ast: str) -> str:
     return parsed_ast
 
 
-def string_placeholders(parsed_ast: str) -> tuple[str, list[str]]:
+def value_placeholders(parsed_ast: str) -> tuple[str, list[str]]:
     """Replace multi-word strings with placeholders $str0$, $str1$, $str2$,..."""
     strings = []
-    while (match := re.search(r'\'(.* .*)\'', parsed_ast)):
-        parsed_ast = parsed_ast[:match.start(
-            0)+1] + f'~str{len(strings)}~' + parsed_ast[match.end(0):]
-        strings.append(match.group(1))
+    # while (match := re.search(r'\'(.*)\'', parsed_ast)):
+    while (match := re.search(r'{value:([^\(][^}]*)}', parsed_ast)):
+        parsed_ast = parsed_ast[:match.start(0)+7] + f'({len(strings)})' + parsed_ast[match.end(0)-1:]
+        value = match.group(1)
+        # If string, use double quotation marks
+        if value[0] == '\'' and value[-1] == '\'':
+            value = f'\"{value[1:-1]}\"'
+        strings.append(value)
 
     return parsed_ast, strings
 
 
-def replace_strings(parsed_ast: str, strings: list[str]) -> str:
+def replace_values(parsed_ast: str, strings: list[str]) -> str:
     """Replace string placeholders with their original strings."""
     for i, string in enumerate(strings):
-        parsed_ast = re.sub(f'~str{i}~', string, parsed_ast)
+        parsed_ast = re.sub(f'"\({i}\)"', string, parsed_ast)
     return parsed_ast
 
 
@@ -80,11 +84,13 @@ def _ast_to_dict(parsed_ast: str) -> str:
     parsed_ast = re.sub(r'\(', r'{', parsed_ast)
     parsed_ast = re.sub(r'\)', r'}', parsed_ast)
     parsed_ast = re.sub(r'{}', r'', parsed_ast)
-    parsed_ast, strings = string_placeholders(parsed_ast)
+    # Temp extract any literal values
+    parsed_ast, values = value_placeholders(parsed_ast)
     parsed_ast = re.sub('\'', '', parsed_ast)
-    parsed_ast = re.sub(r'([\w.\(\)~]+)', r'"\1"', parsed_ast)
-    parsed_ast = replace_strings(parsed_ast, strings)
-    parsed_ast = re.sub(r'"([\d.]+)"', r"\1", parsed_ast)
+    # Apply double quotation marks to all words
+    parsed_ast = re.sub(r'([\w.\(\)]+)', r'"\1"', parsed_ast)
+    # Replace extracted literal values
+    parsed_ast = replace_values(parsed_ast, values)
     parsed_ast = re.sub(r'"{', r'":{', parsed_ast)
 
     return parsed_ast
@@ -94,9 +100,9 @@ def ast_to_dict(node_or_string) -> dict:
     """Converts an AST in node or string format into a dictionary."""
     if type(node_or_string) != str:
         node_or_string = ast.dump(node_or_string, indent=1)
-
+    
     node_or_string = _ast_to_dict(node_or_string)
-
+    
     try:
         d = json.loads(node_or_string)
     except json.decoder.JSONDecodeError:
--- tests/tests.py
+++ tests/tests.py
@@ -2,8 +2,8 @@ import sys
 import os
 
 sys.path.insert(0, os.getcwd() + '/..')
-import ast
 import pytest
+import ast
 from export_ast import ast_to_dict
 
 
@@ -27,3 +27,239 @@ def test_code(code, expected):
     assert got2 == expected
 
     assert got1 == got2
+
+
+@pytest.mark.parametrize('file, expected', [('file1', {
+    "Module": {
+        "body": [
+            {
+                "ImportFrom": {
+                    "module": "setuptools",
+                    "names": [
+                        {
+                            "alias": {
+                                "name": "setup"
+                            }
+                        },
+                        {
+                            "alias": {
+                                "name": "find_packages"
+                            }
+                        }
+                    ],
+                    "level": "0"
+                }
+            },
+            {
+                "Assign": {
+                    "targets": [
+                        {
+                            "Name": {
+                                "id": "long_description",
+                                "ctx": "Store"
+                            }
+                        }
+                    ],
+                    "value": {
+                        "Call": {
+                            "func": {
+                                "Attribute": {
+                                    "value": {
+                                        "Call": {
+                                            "func": {
+                                                "Name": {
+                                                    "id": "open",
+                                                    "ctx": "Load"
+                                                }
+                                            },
+                                            "args": [
+                                                {
+                                                    "Constant": {
+                                                        "value": "README.md"
+                                                    }
+                                                }
+                                            ],
+                                            "keywords": []
+                                        }
+                                    },
+                                    "attr": "read",
+                                    "ctx": "Load"
+                                }
+                            },
+                            "args": [],
+                            "keywords": []
+                        }
+                    }
+                }
+            },
+            {
+                "Expr": {
+                    "value": {
+                        "Call": {
+                            "func": {
+                                "Name": {
+                                    "id": "setup",
+                                    "ctx": "Load"
+                                }
+                            },
+                            "args": [],
+                            "keywords": [
+                                {
+                                    "keyword": {
+                                        "arg": "name",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "export-ast"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "version",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "1.0.5"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "author",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "Tom Draper"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "author_email",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "tomjdraper1@gmail.com"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "license",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "MIT"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "description",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "Export a Python AST to a dictionary."
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "long_description",
+                                        "value": {
+                                            "Name": {
+                                                "id": "long_description",
+                                                "ctx": "Load"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "long_description_content_type",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "text/markdown"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "url",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "https://github.com/tom-draper/ast-export"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "key_words",
+                                        "value": {
+                                            "Constant": {
+                                                "value": "ast syntax trees export dict json dictionary"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "install_requires",
+                                        "value": {
+                                            "List": {
+                                                "elts": [],
+                                                "ctx": "Load"
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "packages",
+                                        "value": {
+                                            "Call": {
+                                                "func": {
+                                                    "Name": {
+                                                        "id": "find_packages",
+                                                        "ctx": "Load"
+                                                    }
+                                                },
+                                                "args": [],
+                                                "keywords": []
+                                            }
+                                        }
+                                    }
+                                },
+                                {
+                                    "keyword": {
+                                        "arg": "python_requires",
+                                        "value": {
+                                            "Constant": {
+                                                "value": ">:3.6"
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        ],
+        "type_ignores": []
+    }
+})])
+def test_code_file(file, expected):
+    with open(f'{file}.py', 'r') as f:
+        tree = ast.parse(f.read())
+        got1 = ast_to_dict(tree)
+        assert got1 == expected
+
+        tree_str = ast.dump(tree, indent=2)
+        got2 = ast_to_dict(tree_str)
+        assert got2 == expected
+
+        assert got1 == got2
